825287efdc63b5cea8749d57581c308c3f738b7a
TRudenko22 <timothy.rudenkom@gmail.com>
2023-03-15 15:27:14 -0400

    added standard boilerplate to templates

--- main.go
+++ main.go
@@ -1,54 +1,69 @@
 package main
 
 import (
-  "embed"
-  "fmt"
-  "log"
-  "os"
+	"embed"
+	"fmt"
+	"log"
+	"os"
+    "strings"
 
-  "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 )
 
+//TODO: add jobs to templates
+//TODO: create -o flag to generate a file
+//TODO: add jobs to templates
+
 var (
-  //go:embed templates/*
-  templates embed.FS 
+	//go:embed templates/*
+	templates embed.FS
 )
 
 func generate(ctx *cli.Context) error {
-  file := ctx.Args().Get(0)
-  
-  template, err := templates.ReadFile(fmt.Sprintf("templates/%s.yaml", file))
-  if err != nil {
-    return fmt.Errorf("Template not found")
-  } else {
-    fmt.Println(string(template))
-    return nil
-  } 
+	file := ctx.Args().Get(0)
+
+	template, err := templates.ReadFile(fmt.Sprintf("templates/%s.yaml", file))
+	if err != nil {
+		return fmt.Errorf("Template not found")
+	} else {
+		fmt.Println(string(template))
+		return nil
+	}
 }
 
 func list(ctx *cli.Context) error {
-  return nil
+    dir, _ := templates.ReadDir("templates")
+    
+    fmt.Println("The following templates are in this binary:")
+    fmt.Println("-------------------------------------------")
+    for _, file := range dir {
+        resource := strings.Split(file.Name(), ".")[0]
+        fmt.Println(resource)
+    }
+    fmt.Println("-------------------------------------------")
+
+	return nil
 }
 
 func main() {
-  app := &cli.App{
-    Commands: []*cli.Command{
-      {
-        Name: "make",
-        Aliases: []string{"m", "mk"},
-        Usage: "Generates a resource template",
-        Action: generate,
-      },
-      {
-        Name: "list",
-        Aliases: []string{"l"},
-        Usage: "Lists available templates",
-        Action: list,
-      },
-    },
-  }
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "make",
+				Aliases: []string{"m", "mk"},
+				Usage:   "Generates a resource template",
+				Action:  generate,
+			},
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "Lists available templates",
+				Action:  list,
+			},
+		},
+	}
 
-  if err := app.Run(os.Args); err != nil {
-    log.Fatal(err)
-  }
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
